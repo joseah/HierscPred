4f74418de7c0a223c24101da7ef4b8feb9df9432
lcmmichielsen <lieke.michielsen@hotmail.com>
2021-04-14 18:58:25 +0200

    Update dimred.R

--- R/dimred.R
+++ R/dimred.R
@@ -1,13 +1,17 @@
-do_pca <- function(data, verbose = FALSE){
+do_pca <- function(data, verbose = FALSE, scaledata = FALSE){
 
   if(ncol(data) < 50){
     npcs <- ncol(data) - 2
   } else {
     npcs <- 50
   }
+  
+  if (scaledata){
+    data <- FindVariableFeatures(data, verbose = verbose)
+    data <- ScaleData(data, verbose = verbose)
+    
+  }
 
-  data <- FindVariableFeatures(data, verbose = verbose)
-  data <- ScaleData(data, verbose = verbose)
   data <- RunPCA(data, npcs = npcs, verbose = verbose, seed.use = 42)
   data
 }
